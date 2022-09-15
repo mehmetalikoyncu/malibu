603eae396849de42659fbe063cc6b56d4965228e
mehmetalikoyncu <113204477+mehmetalikoyncu@users.noreply.github.com>
2022-09-15 14:27:26 +0300

    Add files via upload

---
+++ settings/ayarlar.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+
+module.exports = {
+    TOKEN: process.env.TOKEN || "MTAxOTU2NjIxNDYxNzUxNDAxNQ.GwLNk_.MMhYrcxqzrRljX1LwN3B1s1uQlPW4kM2YRt-FE",  // bot token gir :)
+    PREFIX: process.env.PREFIX || "mali", // bot prefix
+    SAHİP: process.env.SAHİP || "332180419099361290", //sahip id :()
+}
