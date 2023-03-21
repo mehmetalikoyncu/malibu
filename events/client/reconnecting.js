0f159f1cbccb6adddeb3eb5e336f82959f28edfc
mehmetalikoyncu <113204477+mehmetalikoyncu@users.noreply.github.com>
2023-03-21 20:28:29 +0300

    Delete events directory

--- events/client/reconnecting.js
+++
@@ -1,5 +0,0 @@
-const chalk = require("chalk");
-
-module.exports = client => {
-    console.log(chalk.yellow(`[${client.user.username}] || Yeniden bağlanıyor ${new Date()}.`))
-}
--- events/distube/searchCancel.js
+++
@@ -1,3 +0,0 @@
-module.exports = async (client, message, query) => {
-    message.channel.send(`hadi be bilader Kasparov musun :D`);
-}
