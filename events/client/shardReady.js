40993a10e7039c4818f729d1aacf7c22b6b8268d
mehmetalikoyncu <113204477+mehmetalikoyncu@users.noreply.github.com>
2023-03-21 20:20:30 +0300

    Update shardReady.js

--- events/client/shardReady.js
+++ events/client/shardReady.js
@@ -5,5 +5,3 @@ module.exports = async (client, id) => {
     await delay(2000); 
     console.log(chalk.greenBright(`[${String(new Date).split(" ", 5).join(" ")}] || ==> || Shard #${id} Hazır`))
 }
-
-// github.com/EmirhanSarac/discord-v14-muzik-botu - discord.gg/codare - youtube.com/EmirhanSarac
