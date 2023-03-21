1ed6067a06f13825b65e5388f72c6fe64dc9a259
mehmetalikoyncu <113204477+mehmetalikoyncu@users.noreply.github.com>
2023-03-21 20:19:49 +0300

    Update shardDisconnect.js

--- events/client/shardDisconnect.js
+++ events/client/shardDisconnect.js
@@ -3,5 +3,3 @@ const chalk = require("chalk");
 module.exports = (client, event, id) => {
     console.log(chalk.redBright(`[${String(new Date).split(" ", 5).join(" ")}] || ==> || Shard #${id} Bağlantı Kesildi`))
 }
-
-// github.com/EmirhanSarac/discord-v14-muzik-botu - discord.gg/codare - youtube.com/EmirhanSarac
