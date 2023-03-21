d1d79fb8b7af2d82f4664e8d0278bff7c63489a5
mehmetalikoyncu <113204477+mehmetalikoyncu@users.noreply.github.com>
2023-03-21 21:17:02 +0300

    Delete settings directory

--- settings/ayarlar.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config();
-
-module.exports = {
-    TOKEN: process.env.TOKEN || "",  // bot token gir :)
-    PREFIX: process.env.PREFIX || "mali", // bot prefix
-    SAHİP: process.env.SAHİP || "1019566214617514015", //sahip id :()
-}
