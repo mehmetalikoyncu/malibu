1a99166fb09e1bd1a033e6314ec72918c8c8dcdc
mehmetalikoyncu <113204477+mehmetalikoyncu@users.noreply.github.com>
2023-03-21 20:20:06 +0300

    Update shardError.js

--- events/client/shardError.js
+++ events/client/shardError.js
@@ -3,4 +3,3 @@ const chalk = require("chalk");
 module.exports = (client, error, id) => {
     console.log(chalk.red(`[${String(new Date).split(" ", 5).join(" ")}] || ==> || Shard #${id} Hata`))
 }
-// github.com/EmirhanSarac/discord-v14-muzik-botu - discord.gg/codare - youtube.com/EmirhanSarac
