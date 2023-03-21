79241950ad2b0412453e50e94387a05c160c24f9
mehmetalikoyncu <113204477+mehmetalikoyncu@users.noreply.github.com>
2023-03-21 21:28:58 +0300

    Update ayarlar.js

--- settings/ayarlar.js
+++ settings/ayarlar.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 module.exports = {
-    TOKEN: process.env.TOKEN || "MTAxOTU2NjIxNDYxNzUxNDAxNQ.GSzRuO.XHbKAGdJTl6pR_kMx9LpHZNt0-T6Bbt3zfTT1Q",  // bot token gir :)
+    TOKEN: process.env.TOKEN || "",  // bot token gir :)
     PREFIX: process.env.PREFIX || "mali", // bot prefix
-    SAHİP: process.env.SAHİP || "1019566214617514015", //sahip id :()
+    SAHİP: process.env.SAHİP || "332180419099361290", //sahip id :()
 }
